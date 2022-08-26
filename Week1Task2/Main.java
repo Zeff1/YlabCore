e0d01d42537060feb07bf1405971057a1ac93e92
Zeff <103216637+Zeff1@users.noreply.github.com>
2022-08-26 22:09:37 +0300

    2

---
+++ Week1Task2/Main.java
@@ -0,0 +1,33 @@
+package Week1Task2;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[]{5, 6, 3, 2, 5, 1, 4, 9};
+        print(array);
+        boolean needSorting = true;
+        while (needSorting) {
+            needSorting = false;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] < array[i - 1]) {
+                    swap(array, i, i - 1);
+                    needSorting = true;
+                }
+            }
+        }
+        print(array);
+    }
+
+
+    public static void swap(int[] array, int a, int b) {
+        int tmp = array[a];
+        array[a] = array[b];
+        array[b] = tmp;
+    }
+
+    public static void print(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+}
