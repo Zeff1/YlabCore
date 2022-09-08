e6f983f244ce75981e11db64fa38b151b7f44ffb
Zeff <103216637+Zeff1@users.noreply.github.com>
2022-09-09 00:39:23 +0300

    week2.2
    task2 completed

--- Week2Task2/Main.java
+++ Week2Task2/Main.java
@@ -7,4 +7,18 @@ package Week2Task2;
          */
 
 public class Main {
+    public static int[] numArray = {3, 4, 2, 7, 6, 8, 5, 5};
+
+    public static void main(String[] args) {
+        sortByTenSum(numArray);
+    }
+
+    public static void sortByTenSum(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] + array[j] == 10)
+                    System.out.println("[" + array[i] + "," + array[j] + "]");
+            }
+        }
+    }
 }
