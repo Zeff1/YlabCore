fa75ce8a278b1d3cb109916527456b4c63123d0c
Zeff <103216637+Zeff1@users.noreply.github.com>
2022-09-09 00:09:14 +0300

    week2
    task1 completed

---
+++ Week2Task1/Main.java
@@ -0,0 +1,80 @@
+package Week2Task1;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+public class Main {
+
+    static class Person {
+        final int id;
+
+        final String name;
+
+        Person(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Person person)) return false;
+            return getId() == person.getId() && getName().equals(person.getName());
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(getId(), getName());
+        }
+    }
+
+    private static Person[] RAW_DATA = new Person[]{
+            new Person(0, "Harry"),
+            new Person(0, "Harry"), // дубликат
+            new Person(1, "Harry"), // тёзка
+            new Person(2, "Harry"),
+            new Person(3, "Emily"),
+            new Person(4, "Jack"),
+            new Person(4, "Jack"),
+            new Person(5, "Amelia"),
+            new Person(5, "Amelia"),
+            new Person(6, "Amelia"),
+            new Person(7, "Amelia"),
+            new Person(8, "Amelia"),
+    };
+
+    public static void main(String[] args) {
+        System.out.println("Raw data:");
+        System.out.println();
+
+        for (Person person : RAW_DATA) {
+            System.out.println(person.id + " - " + person.name);
+        }
+
+        System.out.println();
+        System.out.println("**************************************************");
+        System.out.println();
+        System.out.println("Duplicate filtered, grouped by name, sorted by name and id:");
+        System.out.println();
+
+        Map<String, Long> dataFiltered = Arrays.stream(RAW_DATA)
+                .filter(Objects::nonNull)
+                .filter(person -> person.getName() != null)
+                .sorted(Comparator.comparingInt(Person::getId))
+                .distinct()
+                .collect(Collectors.groupingBy(Person::getName, Collectors.counting()));
+        for (Map.Entry<String, Long> entry : dataFiltered.entrySet()) {
+            System.out.println("Key: " + entry.getKey());
+            System.out.println("Value: " + entry.getValue());
+        }
+    }
+}
