b763e62452e7ce57b436c30b9cc19f14ed045744
Zeff <103216637+Zeff1@users.noreply.github.com>
2022-09-12 20:24:51 +0300

    corrections

--- Week2/Task2/Main.java
+++ Week2/Task2/Main.java
@@ -6,6 +6,8 @@ package Week2.Task2;
             [3, 4, 2, 7], 10 -> [3, 7] - вывести пару менно в скобках, которые дают сумму - 10
          */
 
+import java.util.Arrays;
+
 public class Main {
     public static int[] numArray = {3, 4, 2, 7, 6, 8, 5, 5};
 
@@ -14,13 +16,17 @@ public class Main {
     }
 
     public static void sortByTenSum(int[] array) {
+        int[] result = new int[2];
         for (int i = 0; i < array.length; i++) {
             for (int j = i + 1; j < array.length; j++) {
-                if (array[i] + array[j] == 10)
-                    System.out.println("[" + array[i] + "," + array[j] + "]");
-                return;
+                if (array[i] + array[j] == 10) {
+                    result[0] = array[i];
+                    result[1] = array[j];
+                    System.out.println(Arrays.toString(result));
+                }
             }
         }
-        System.out.println("Pair not found!");
+        if (result[0] == 0 && result[1] == 0)
+            System.out.println("Pair not found!");
     }
 }
